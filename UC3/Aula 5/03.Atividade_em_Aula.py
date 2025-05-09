b7b82bc8219ffa1009b554519bb2bb47f7a159df
pccordeiro16 <152649165+pccordeiro16@users.noreply.github.com>
2025-05-09 17:45:15 -0300

    Add files via upload

--- UC3/Aula 5/03.Atividade_em_Aula.py
+++ UC3/Aula 5/03.Atividade_em_Aula.py
@@ -20,7 +20,16 @@ class Livro:
         self.ano = ano
 
     def __str__(self):
-        return f'{self.titulo} ({self.autor} - {self.ano})'
+        return f'| {self.titulo} ({self.autor} - {self.ano}) |'
+    
+class LivroDigital(Livro):
+    def __init__(self, titulo, autor, ano, formato, link_download):
+        super().__init__(titulo, autor, ano)
+        self.formato = formato
+        self.link_download = link_download
+
+    def __str__(self):
+        return f'| {self.titulo} (Digital - {self.formato}) | Download: {self.link_download} |'
     
 class Usuario:
     def __init__(self, id, nome, cpf):
@@ -31,21 +40,72 @@ class Usuario:
     def __str__(self):
         return f'| ID: {self.id} | Nome: {self.nome} | CPF: {self.cpf} |'
     
+
+from datetime import datetime
+
 class Emprestimo:
-    def __init__(self, livro, usuario, data, hora):
+    def __init__(self, livro, usuario):
         self.livro = livro
         self.usuario = usuario
-        self.data = data
-        self.hora = hora
+        self.data = datetime.now().strftime('%d/%m/%Y')
+        self.hora = datetime.now().strftime('%H:%M')
 
     def __str__(self):
-        return f'{self.usuario.nome} pegou o livro "{self.livro.titulo}" no dia {self.data}, às {self.hora}.'
+        return f'--- {self.usuario.nome} pegou o livro "{self.livro.titulo}" no dia {self.data}, às {self.hora}.\n'
     
-class LivroDigital(Livro):
-    def __init__(self, titulo, autor, ano, formato, link_download):
-        super().__init__(titulo, autor, ano)
-        self.formato = formato
-        self.link_download = link_download
+class Biblioteca:
+    def __init__(self):
+        self.livros = []
+        self.usuarios = []
+        self.emprestimos = []
 
-    def __str__(self):
-        return f'{self.titulo} (Digital - {self.formato}) - Download: {self.link_download}'
+    def cadastrarLivro(self, livro):
+        self.livros.append(livro)
+
+    def cadastrarUsuario(self, usuario):
+        self.usuarios.append(usuario)
+
+    def emprestarLivro(self, emprestimo):
+        self.emprestimos.append(emprestimo)
+
+    def listarLivros(self):
+        for livro in self.livros:
+            print(livro)
+
+    def listarUsuarios(self):
+        for usuario in self.usuarios:
+            print(usuario)
+
+    def listarEmprestimos(self):
+        for emprestimo in self.emprestimos:
+            print(emprestimo)
+
+mlib = Biblioteca()
+
+book1 = Livro('One Piece Vol. 111', 'Eiichiro Oda', 2025)
+book2 = Livro('Dom Casmurro', 'Machado de Assis', 1900)
+mlib.cadastrarLivro(book1)
+mlib.cadastrarLivro(book2)
+
+user1 = Usuario(900, 'Joaquim da Silva', 12345678901)
+user2 = Usuario(901, 'Kaio Vianna', 97865432110)
+mlib.cadastrarUsuario(user1)
+mlib.cadastrarUsuario(user2)
+
+emp1 = Emprestimo(book1, user1)
+emp2 = Emprestimo(book2, user2)
+mlib.emprestarLivro(emp1)
+mlib.emprestarLivro(emp2)
+
+print('Livros:\n')
+mlib.listarLivros()
+
+print('\n----------------------------------------------------------------------\n')
+
+print('Clientes:\n')
+mlib.listarUsuarios()
+
+print('\n----------------------------------------------------------------------\n')
+
+print('Empréstimos realizados:\n')
+mlib.listarEmprestimos()
