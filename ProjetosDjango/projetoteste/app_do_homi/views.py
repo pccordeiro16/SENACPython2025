3b9d0573ebb974243fc98a562025fec48f61dbfb
pccordeiro16 <152649165+pccordeiro16@users.noreply.github.com>
2025-05-19 21:27:13 -0300

    Add files via upload

--- ProjetosDjango/projetoteste/app_do_homi/views.py
+++ ProjetosDjango/projetoteste/app_do_homi/views.py
@@ -1,9 +1,46 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Produto, UserProfile
+from django.conf import settings
+from .forms import ProdutoForm, ProfileForm
+import os
 
 def home(request):
-    return render(request, 'home.html')
+    return render(request, 'app_do_homi/home.html')
+
 def produtos(request):
-    return render(request, 'produtos.html')
+    produtos = Produto.objects.filter(disponivel=True)
+    return render(request, 'app_do_homi/produtos.html', {'produtos': produtos})
+
 def contatos(request):
-    return render(request, 'contatos.html')
+    return render(request, 'app_do_homi/contatos.html')
+
+
+def list_profile_pics(request):
+    """
+    Lista todas as imagens de perfil usando a OS Library.
+    """
+    pics_path = os.path.join(settings.MEDIA_ROOT, 'profile_pics')
+    pictures = [f for f in os.listdir(pics_path) if f.endswith(('.jpg', '.png'))]
+    return render (request, 'list_pics.html', {'pictures': pictures})
+
+def add_produto(request):
+    if request.method == 'POST':
+        form = ProdutoForm (request.POST, request.FILES)  # Note o request.FILES!
+        if form.is_valid():
+            form.save()
+            return redirect('lista_produtos')
+    else:
+        form = ProdutoForm()
+    return render(request, 'app_do_homi/add_produto.html', {'form': form})
+
+def upload_profile(request):
+    if request.method == 'POST':
+        form = ProfileForm(request.POST, request.FILES)
+        if form.is_valid():
+            profile = form.save(commit=False)
+            profile.user = request.user
+            profile.save()
+            return redirect('home')
+    else:
+        form = ProfileForm()
+    return render(request, 'meuapp/upload_profile.html', {'form': form})
